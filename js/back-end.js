16f00699caf54308ff884de933f1f8c5b2f38510
emaravv <guilherme.2021oliveira0@gmail.com>
2023-10-20 20:10:14 -0400

    Ending...

--- js/back-end.js
+++ js/back-end.js
@@ -4,5 +4,5 @@ Abrir = () =>{
   document.getElementById('button').style.display = 'none';
   document.getElementById('ocult').style.display = 'initial';
   document.getElementById('box').style.height = '390px';
-  document.getElementById('box').style.width = '410px';
+  document.getElementById('box').style.width = '400px';
 }
